8d87049261fca231403cce9de4914eb891d51c94
Pavel <fun.love.nuts@gmail.com@>
2022-07-27 10:35:10 +0300

    sdf

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,17 @@
 const { readFileSync } = require('fs');
 const { createServer } = require("@graphql-yoga/node");
 const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const { PrismaClient } = require('@prisma/client/edge')
+
+
+const prisma = new PrismaClient()
 
 const typeDefs = readFileSync(require.resolve('./schema.graphql')).toString('utf-8')
 
 const resolvers = {
   Query,
+  Mutation
 };
 
 const server = createServer({
@@ -13,6 +19,7 @@ const server = createServer({
     typeDefs,
     resolvers,
   },
+  context: { prisma }
 });
 
 server.start();
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -2,11 +2,13 @@
 
 const version = () => '1.4.8.8'
 
-const products = (_parent, _args, _context, _info) => {
+const products = async (_parent, _args, context, _info) => {
   const foundProducts = [
     { id: 1, title: 'Product 1', price: 228 },
     { id: 2, title: 'Product 2', price: 822 }
   ]
+
+  // const foundProducts = await context.prisma.product.findMany()
 
   return foundProducts
 }
