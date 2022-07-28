b4ca64b484736474688b31260df230e68715453e
Pavel <fun.love.nuts@gmail.com@>
2022-07-28 06:42:15 +0300

    Changed db

--- server/src/resolvers/Message.js
+++ server/src/resolvers/Message.js
@@ -17,6 +17,7 @@ const comment = (_parent, args, context, _info) =>
 
 async function createMessage(_parent, args, context, _info) {
   const createdMessage = await context.prisma.message.create({data: args.message})
+  console.log(args.message)
   context.pubSub.publish('NEW_MESSAGE', createdMessage);
   return createdMessage
 }
